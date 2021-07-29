ddbd87a47d48f70ddea16b2da2b8aeb10bdc6c75
kesha1225 <samedov03@mail.ru>
2021-07-29 11:11:06 +0300

    Revert "typeerror with heapq fixed"
    This reverts commit d1a832cabd1dd2cb19054d4a9d991f4a09bf5eb4.

--- event-loop-yield-from.py
+++ event-loop-yield-from.py
@@ -34,25 +34,11 @@ class Future:
         if self.exception:
             raise self.exception
         return self.result
-
+        
     def __iter__(self):
         while not self.done:
             yield None
         return self.get_result()
-
-
-class HeapFuture:
-    def __init__(self, absolute_time, result_future, callable_future):
-        self.absolute_time = absolute_time
-        self.result_future = result_future
-        self.callable_future = callable_future
-        self.data = (self.absolute_time, self.result_future, self.callable_future)
-
-    def __getitem__(self, item):
-        return self.data[item]
-
-    def __gt__(self, other: "HeapFuture"):
-        return self.absolute_time > other.absolute_time
 
 
 class CallbackHandler:
@@ -63,7 +49,7 @@ class CallbackHandler:
         future = Future()
         heapq.heappush(
             self.heap,
-            HeapFuture(
+            (
                 delay + monotonic(),    # absolute time
                 future,                 # result future
                 partial(func, *args)    # callable
@@ -75,7 +61,7 @@ class CallbackHandler:
         while self.heap:
             if self.heap[0][0] > monotonic():
                 return
-            soon, future, func = heapq.heappop(self.heap).data
+            soon, future, func = heapq.heappop(self.heap)
             try:
                 future.set_result(func())
             except Exception as e:
